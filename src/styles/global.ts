54138167b4b0128a18e01b557de2d6d936b2f166
Davi Barreto <daviarturdesa686@gmail.com>
2024-01-12 12:47:05 -0300

    Configure portfolio

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #1D3557;
+    color: #F1FAEE;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input-security, button {
+    font: 16px 'Roboto Slab', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
