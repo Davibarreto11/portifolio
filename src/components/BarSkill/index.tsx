d31a9d563acab628527363a7a62612f0b4e27aaf
Davi Barreto <daviarturdesa686@gmail.com>
2024-01-12 18:32:22 -0300

    Barskill

---
+++ src/components/BarSkill/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const BarSkill: React.FC = () => {
+  return (
+    <ul className='w-full flex flex-wrap items-center justify-between text-lg sm:text-lg md:text-xl xl:text-2xl 2xl:text-3xl'>
+      <li className=''>ReactJS</li>
+      <li className=''>Node.js</li>
+      <li className=''>PHP</li>
+      <li className=''>Laravel</li>
+      <li className=''>Javascript</li>
+      <li className=''>Typescript</li>
+      <li className=''>React Native</li>
+    </ul>
+  )
+}
