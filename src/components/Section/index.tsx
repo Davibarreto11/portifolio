b3bf434018f99dcd44d46e6bc97997d0f3ed1e5b
Davi Barreto <daviarturdesa686@gmail.com>
2024-02-15 16:46:52 -0300

    commit

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -9,7 +9,7 @@ const Section: React.FC<SectionProps> = ({ children, level }) => {
   return (
     <div
       id={level}
-      className={`flex justify-center min-h-screen mt-4 ${level === 'barSkill' ? 'bg-[#457B9D] min-h-[auto]' : ''}`}
+      className={`flex justify-center min-h-screen mt-4 ${level === 'barSkill' ? 'bg-[#457B9D] min-h-[10vh]' : ''}`}
     >
       <div className='flex' style={{ width: '80%' }}>
         {children}
